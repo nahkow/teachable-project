1edf617714efe1f7fe2a5757b5b7d4190ae0cfdf
Natasha Kowalsetskyj <natashakowalsetskyj@Natashas-MacBook-Pro.local>
2024-02-08 14:07:09 -0300

    fixing deploy

--- src/app/api/courses/enrollments/route.ts
+++ src/app/api/courses/enrollments/route.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-const createPath = (courseId) => `https://developers.teachable.com/v1/courses/${courseId}/enrollments`
+const createPath = (courseId: string) =>
+  `https://developers.teachable.com/v1/courses/${courseId}/enrollments`;
 
-export async function GET(request: Request){
-  const {searchParams} = new URL(request.url)
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
   const id = searchParams.get("course_id");
   const res = await fetch(createPath(id), {
     headers: {
-      'Content-Type': 'application/json',
-      'apiKey': process.env.DATA_API_KEY,
+      "Content-Type": "application/json",
+      apiKey: process.env.DATA_API_KEY,
     },
-  })
-  const data = await res.json()
- 
-  return NextResponse.json({ "enrollments": data.enrollments })
+  });
+  const data = await res.json();
 
+  return NextResponse.json({ enrollments: data.enrollments });
 }
